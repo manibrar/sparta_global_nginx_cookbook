ff5a8763762a6f62812b29550faa309fecbb4810
manibrar <manvirbrar1988@gmail.com>
2018-07-31 00:38:29 +0100

    all files

--- nginx/spec/unit/recipes/default_spec.rb
+++ nginx/spec/unit/recipes/default_spec.rb
@@ -7,6 +7,18 @@
 require 'spec_helper'
 
 describe 'nginx::default' do
+  context 'When all attributes are default, on Ubuntu 16.04' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
 
   context 'When all attributes are default, on Ubuntu 16.04' do
     let(:chef_run) do
@@ -20,8 +32,7 @@ describe 'nginx::default' do
       expect { chef_run }.to_not raise_error
     end
     it 'installs nginx' do
-      expect( chef_run ).to install_package('nginx')
+      expect { chef_run }.to install_package 'nginx'
     end
-
   end
 end
